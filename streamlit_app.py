9c3f5083dab9a16598adcaf8381997c468c97188
Andy Block <ablock91@gmail.com>
2021-12-04 18:06:38 -0500

    finished the app, radio button clearnup would be nice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,19 @@
 import streamlit as st
 
 GROSS_MONTHLY: int
-LEASE_LENGTH: float = 12.0
-CONCESSIONS: bool = False
-MONTHS_OFF: float = 0.0
-BROKERS_FEE: bool = False
-PERCENTAGE: float = 0.0
+LEASE_LENGTH: float
+CONCESSIONS: bool
+MONTHS_OFF: float
+BROKERS_FEE: bool
+PERCENTAGE: float
 
-def calculate_net_monthly(GROSS_MONTHLY, LEASE_LENGTH, CONCESSIONS, MONTHS_OFF, BROKERS_FEE, PERCENTAGE):
-    net_monthly = GROSS_MONTHLY
+def calculate_net_monthly(gross_monthly, lease_length, concessions, months_off, brokers_fee, percentage):
+    net_total = gross_monthly*lease_length
 
-    if CONCESSIONS:
-        net_monthly = GROSS_MONTHLY*(LEASE_LENGTH-MONTHS_OFF)/LEASE_LENGTH
+    if concessions: net_total -= gross_monthly*months_off
+    if brokers_fee: net_total += gross_monthly*12*(percentage/100)
 
-    # if BROKERS_FEE:
-    #     net_monthly
-
-    return net_monthly
+    return net_total/lease_length
 
 header = st.container()
 init_terms_container = st.container()
@@ -27,8 +24,8 @@ net_cost_container = st.container()
 with header:
     st.title('Apartment Cost Calculator')
     st.write("""
-        This app computes the net cost of renting an apartment in NY.  Required are the gross cost per month and any
-        concessions and/or broker's fee involving in the rental.
+        This app computes the net cost of renting an apartment in NY.  Required are the gross cost per month and
+        lease length.  Optional inputs are any concessions and/or broker's fees involved in the rental.
         """)
     st.write('')
 
@@ -40,16 +37,19 @@ with init_terms_container:
 with concessions_container:
     concession_toggle, months_off_input = st.columns(2)
     concession_bool = concession_toggle.radio('Concessions?', ('No', 'Yes'))
-    if concession_bool == 'Yes': CONCESSIONS is True
+    concession_toggle.write('<style>div.row-widget.stRadio > div{flex-direction:row; float:left; padding: 5px;}</style>', unsafe_allow_html=True)
+    CONCESSIONS = True if concession_bool == 'Yes' else False
 
     MONTHS_OFF = months_off_input.number_input('Months off:', min_value=0.0, step=0.25, format='%f')
 
 with brokers_fee_container:
     brokers_fee_toggle, percentage_input = st.columns(2)
     brokers_fee_bool = brokers_fee_toggle.radio('Brokers Fee?', ('No', 'Yes'))
-    if brokers_fee_bool == 'YES': BROKERS_FEE is True
+    BROKERS_FEE = True if brokers_fee_bool == 'Yes' else False
 
     PERCENTAGE = percentage_input.number_input('Percentage:', value=0.0, step=0.5, format='%f')
 
 with net_cost_container:
-    calculate_net_monthly(GROSS_MONTHLY, LEASE_LENGTH, CONCESSIONS, MONTHS_OFF, BROKERS_FEE, PERCENTAGE)
+    net_monthly = calculate_net_monthly(GROSS_MONTHLY, LEASE_LENGTH, CONCESSIONS, MONTHS_OFF, BROKERS_FEE, PERCENTAGE)
+    st.subheader('Net monthly cost is:')
+    st.write(f'${net_monthly:0.2f}')
